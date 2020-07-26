bd1a8952397d66583e91708d04ede5c7995e2778
daff <daffmg1@gmail.com>
2020-07-26 16:51:42 -0500

    debugging

---
+++ src/debug/index.js
@@ -0,0 +1,12 @@
+var a ='Hello';
+function hello(){
+    let b = 'Hello World';
+    const c = 'Hello Wordl!'
+
+    if (true) {
+        let d = 'Ello world!!'
+        debugger
+    }
+}
+
+hello();
