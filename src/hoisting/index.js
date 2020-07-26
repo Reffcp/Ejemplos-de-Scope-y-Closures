4bf57633663bb507d9a5c0a1353f65c285aae0ce
daff <daffmg1@gmail.com>
2020-07-26 16:46:34 -0500

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,15 @@
+//no declaradas siempre son var
+a=2;
+var a;
+console.log(a);
+
+//elevacion y se declara a como undeined
+console.log(a);
+var a =2;
+
+//el hoistin define el function primero
+nameOfDog('Morgan')
+function nameOfDog(name){
+    console.log(name);
+}
+nameOfDog('Morgan')
